872cd52504d12a0581e0559cdb88a6e72c127a00
Michael Ye <34128025+michaellikaiye@users.noreply.github.com>
2018-10-09 17:03:58 -0400

    ;/ ;(

--- list.c
+++ list.c
@@ -6,9 +6,12 @@ struct node {int price; struct node *next;};
 
 /* Should take a pointer to a node struct and print out all of the data in the list */
 void print_list(struct node *n) {
-  printf("%d, ",n->price);
-  if(n->next)
-    print_list(n->next);
+  if(!n)
+    printf("null");
+  while(n) {
+    printf("%d, ",n->price);
+    n = n->next;
+  }
 }
 
 /* Should take a pointer to the existing list and the data to be added, create a new node and put it at the beginning of the list. */
@@ -24,24 +27,54 @@ struct node * insert_front(struct node *n, int a) {
 
 /* Should take a pointer to a list as a parameter and then go through the entire list freeing each node and return a pointer to the beginning of the list (which should be NULL by then).   */
 struct node * free_list(struct node *n) {
+  //struct node *p = n;
+  //while(n) {
+    //struct node *c = n->next;
+    //free(n);
+    //n = c;
+    //printf("%d, ",c->price);
+  //}
+  while(n) {
+    free(n);
+  }
+  return n;
+}
+void freeList(struct node* n)
+{
+   struct node *p;
+
+   while (n != NULL)
+    {
+       p = n;
+       n = n->next;
+       free(p);
+    }
 
 }
-
 int main() {
-  struct node *s1;
-  struct node *s2;
-  struct node *s3;
-  s1->price = 91;
-  s1->next = s2;
-  s2->price = 19;
-  s2->next = s3;
-  s3->price = 44;
-  s3->next = NULL;
+  struct node a;
+  struct node b;
+  struct node c;
+  struct node *s1 = &a;
+  struct node *s2 = &b;
+  struct node *s3 = &c;
+  a.price = 91;
+  a.next = s2;
+  b.price = 19;
+  b.next = s3;
+  c.price = 44;
+  c.next = NULL;
 
   printf("List starting from s1:\n");
   print_list(s1);
   printf("\n\nAdding 5 to beginning\n");
   struct node *s0 = insert_front(s1, 5);
-  print_list(s0);
+  struct node *sa = insert_front(s0, 7);
+  struct node *sb = insert_front(sa, 9);
+  print_list(sb);
+  printf("\nfree\n");
+  freeList(sb);
+  //struct node *sx = free_list(sb);
+  //print_list(sb);
   return 0;
 }
